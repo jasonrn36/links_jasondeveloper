0f3bdf9ff024e86bf8651d1a18eaea4a07b2b2df
jsonrn36 <jason.rn36@gmail.com>
2026-04-22 22:55:01 -0400

    adicionado os produtos e modificada a loja

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement(
-        {
-            pageLanguage: 'pt',        // idioma original da página
-            includedLanguages: 'en,es', // idiomas permitidos (ex: inglês e espanhol)
-            autoDisplay: true          // força tradução automática
-        },
-        'google_translate_element'
-        );
+function filtrarProdutos() {
+    let input = document.getElementById('inputBusca').value.toLowerCase();
+    let cards = document.getElementsByClassName('produto-card');
+
+    for (let i = 0; i < cards.length; i++) {
+        let nome = cards[i].querySelector('.nome').innerText.toLowerCase();
+        if (nome.includes(input)) {
+            cards[i].style.display = "flex";
+        } else {
+            cards[i].style.display = "none";
+        }
     }
+}
